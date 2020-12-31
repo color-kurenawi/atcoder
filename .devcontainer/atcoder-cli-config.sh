3231b2a5edba5d5f10cda883ae655dc40d65f776
color-kurenawi <70028074+color-kurenawi@users.noreply.github.com>
2020-12-31 20:23:28 +0900

    Added atcoder-cli settings

---
+++ .devcontainer/atcoder-cli-config.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# atcoder-cliの設定をするためのshell
+
+# 入出力のディレクトリの設定をonline-judge-toolsと合わせる
+acc config default-test-dirname-format test
+
+# デフォルトですべての問題をダウンロードする．
+acc config default-task-choice all
+
+# pythonのテンプレートファイルを設定する．
+acc_config_dir=$(acc config-dir)
+ln -s /atcoder/templates/python $acc_config_dir/python
+
+# デフォルトのテンプレートをpythonに設定する．
+acc config default-template python

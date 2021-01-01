9b699ad6bf5325c0f9b652ea7eb58b6fc2c9038e
color-kurenawi <70028074+color-kurenawi@users.noreply.github.com>
2021-01-02 00:04:40 +0900

    Added oj-tools test alias

--- .devcontainer/atcoder-cli-config.sh
+++ .devcontainer/atcoder-cli-config.sh
@@ -15,5 +15,9 @@ ln -s /atcoder/templates/python $acc_config_dir/python
 # デフォルトのテンプレートをpythonに設定する．
 acc config default-template python
 
+# テストを簡単化するためのaliasを設定する
+echo "alias oj-t='oj t -c \"python3.8 main.py ONLINE_JUDGE\"'" >> ~/.bash_aliases
+echo "alias oj-t-pypy='oj t -c \"pypy3 main.py\"'" >> ~/.bash_aliases
+
 # pypyの提出用にaliasを設定する
 echo 'alias acc-s-pypy="acc s main.py -- --guess-python-interpreter pypy"' >> ~/.bash_aliases

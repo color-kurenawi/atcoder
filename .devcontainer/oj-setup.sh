25eef3f8191732da849814be2568a1400d7227d8
color-kurenawi <70028074+color-kurenawi@users.noreply.github.com>
2021-01-05 23:23:56 +0900

    Added oj-download setup

--- .devcontainer/oj-setup.sh
+++ .devcontainer/oj-setup.sh
@@ -13,6 +13,7 @@ ln -s /atcoder/.devcontainer/prepare.config.toml ~/.config/online-judge-tools/
 
 # /usr/local/bin/にラッパーのシンボリックリンクを張る．
 sudo ln -s /atcoder/scripts/oj-new /usr/local/bin/
+sudo ln -s /atcoder/scripts/oj-download /usr/local/bin/
 
 # alias集を~/.bash_aliasesに追加する．
 cat /tmp/oj_aliases >> ~/.bash_aliases
